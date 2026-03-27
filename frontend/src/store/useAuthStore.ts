f1ff32440687f7fc48be9f65f9e62f4c7c265ad4
unknown <lipikasen79@gmail.com>
2026-03-27 22:17:55 +0530

    fixed the socket connection

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import {io} from "socket.io-client"
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";//url of backend
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : import.meta.env.VITE_API_URL;//url of backend
 
 type User = {
   _id: string;
